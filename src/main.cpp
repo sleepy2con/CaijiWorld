08fcc7b30f4ada466b0b304b36c8eedd66be04b4
heyuxuan <heyuxuan_work@qq.com>
2026-03-12 22:54:57 +0800

    让ai帮我写一个SDL3的最小例子，后面慢慢搞

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,115 @@
-#include <iostream>
-int main(){
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
+#include <SDL3/SDL.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
-//#include <SDL2/SDL.h>
-//#include <iostream>
-//
-//#include "sdl2.h"
-//
-//auto main() -> int
-//{
-//    using std::cerr;
-//    using std::endl;
-//
-//    auto sys = sdl2::make_sdlsystem(SDL_INIT_EVERYTHING);
-//    if (!sys) {
-//        cerr << "Error creating SDL2 system: " << SDL_GetError() << endl;
-//        return 1;
-//    }
-//
-//    auto win = sdl2::make_window("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
-//    if (!win) {
-//        cerr << "Error creating window: " << SDL_GetError() << endl;
-//        return 1;
-//    }
-//
-//    auto ren
-//        = sdl2::make_renderer(win.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-//    if (!ren) {
-//        cerr << "Error creating renderer: " << SDL_GetError() << endl;
-//        return 1;
-//    }
-//
-//    auto file = SDL_RWFromFile(IMGDIR "grumpy-cat.bmp", "rb");
-//    if (file == nullptr) {
-//        cerr << "Error reading file: " << SDL_GetError() << endl;
-//        return 1;
-//    }
-//
-//    auto bmp = sdl2::make_bmp(file);
-//    if (!bmp) {
-//        cerr << "Error creating surface: " << SDL_GetError() << endl;
-//        return 1;
-//    }
-//
-//    auto tex = sdl2::make_texture(ren.get(), bmp.get());
-//    if (!tex) {
-//        cerr << "Error creating texture: " << SDL_GetError() << endl;
-//        return 1;
-//    }
-//
-//    for (int i = 0; i < 20; i++) {
-//        SDL_RenderClear(ren.get());
-//        SDL_RenderCopy(ren.get(), tex.get(), nullptr, nullptr);
-//        SDL_RenderPresent(ren.get());
-//        SDL_Delay(100);
-//    }
-//
-//    return 0;
-//}
+struct Unit
+{
+    float x;
+    float y;
+    float vx;
+    float vy;
+};
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Grid Demo",
+        800,
+        600,
+        0
+    );
+
+    SDL_Renderer* renderer =
+        SDL_CreateRenderer(window, NULL);
+
+    const int gridSize = 20;
+    const int mapW = 50;
+    const int mapH = 50;
+
+    std::vector<Unit> units;
+
+    srand(time(nullptr));
+
+    // 创建1000个单位
+    for (int i = 0; i < 1000; i++)
+    {
+        Unit u;
+        u.x = rand() % (mapW * gridSize);
+        u.y = rand() % (mapH * gridSize);
+        u.vx = (rand() % 3 - 1) * 0.5f;
+        u.vy = (rand() % 3 - 1) * 0.5f;
+        units.push_back(u);
+    }
+
+    bool running = true;
+
+    while (running)
+    {
+        SDL_Event e;
+
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_EVENT_QUIT)
+                running = false;
+        }
+
+        // 更新单位
+        for (auto& u : units)
+        {
+            u.x += u.vx;
+            u.y += u.vy;
+
+            if (u.x < 0 || u.x > mapW * gridSize)
+                u.vx = -u.vx;
+
+            if (u.y < 0 || u.y > mapH * gridSize)
+                u.vy = -u.vy;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+        SDL_RenderClear(renderer);
+
+        // 画网格
+        SDL_SetRenderDrawColor(renderer, 60, 60, 60, 255);
+
+        for (int x = 0; x <= mapW; x++)
+        {
+            SDL_RenderLine(
+                renderer,
+                x * gridSize,
+                0,
+                x * gridSize,
+                mapH * gridSize
+            );
+        }
+
+        for (int y = 0; y <= mapH; y++)
+        {
+            SDL_RenderLine(
+                renderer,
+                0,
+                y * gridSize,
+                mapW * gridSize,
+                y * gridSize
+            );
+        }
+
+        // 画单位
+        SDL_SetRenderDrawColor(renderer, 200, 80, 80, 255);
+
+        for (auto& u : units)
+        {
+            SDL_FRect r = { u.x, u.y, 6, 6 };
+            SDL_RenderFillRect(renderer, &r);
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
